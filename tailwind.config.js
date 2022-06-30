10d9f561bb2445000dcd0525bf70443e8c43f882
Abdullah <mdabdullahnoman777@gmail.com>
2022-06-30 21:50:42 +0600

    daisey ui connect

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#FF014F",
+          secondary: "#0D0D0D",
+          neutral: "#ECF0F3",
+          "base-100": "#ECF0F3",
+          info: "#ff014d91",
+          success: "#E449A4",
+          warning: "#DF7E07",
+          error: "#FA5C5C",
+        },
+      },
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
